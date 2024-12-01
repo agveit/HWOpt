e6c90ddfc453072a2c9ee41ca90a351f0c445ec9
agveit <agveit.a@mail.ru>
2024-12-01 20:37:05 +0500

    hw

--- HWOpt/Program.cs
+++ HWOpt/Program.cs
@@ -10,6 +10,20 @@ namespace HWOpt
     {
         static void Main(string[] args)
         {
+            string text = "Это пример текста. Текст должен быть проанализирован. Это важно.";
+            Dictionary<string, int> wordCounts = WordFrequency(text);
+
+            foreach (var item in wordCounts)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+
+        static Dictionary<string, int> WordFrequency(string text)
+        {
+            string[] words = text.ToLower().Split(new[] { ' ', '.'}, StringSplitOptions.RemoveEmptyEntries);
+
+            return words.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
         }
     }
 }
